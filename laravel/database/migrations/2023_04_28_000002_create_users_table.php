123a0a8d68c4ec8d7da1558e0ac5649a7aeabb4b
IDOShun <ido_shun@retail-ai.jp>
2023-04-28 21:20:08 +0900

    latest

--- laravel/database/migrations/2023_04_28_000002_create_users_table.php
+++ laravel/database/migrations/2023_04_28_000002_create_users_table.php
@@ -4,21 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     * @return void
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('password', 50);
+            $table->tinyInteger('CRUD')->default(3);
+            $table->Integer('role_id');
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
