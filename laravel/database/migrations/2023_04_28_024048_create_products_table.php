70f8b5aec91e2e5b0c852185aa3cf2dfddad9a18
IDOShun <ido_shun@retail-ai.jp>
2023-04-28 20:34:50 +0900

    DB構築終了＆Model作成

---
+++ laravel/database/migrations/2023_04_28_024048_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('description', 200)->nullable();
+            $table->string('image', 100)->unique()->default('defaultImg.jpg');
+            $table->char('SKU', 20)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
